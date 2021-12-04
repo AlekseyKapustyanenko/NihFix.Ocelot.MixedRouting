1f39ae925d1a5883bb33602b487ca5dded324d76
ALEKSEY KAPUSTYANENKO <lehkap@hotmail.com>
2021-12-04 14:27:59 +0400

    CreateProvider and ProviderFactory

---
+++ NihFix.Ocelot.MixedRouting/MixedDownstreamRouteProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Configuration;
+using Ocelot.DownstreamRouteFinder;
+using Ocelot.DownstreamRouteFinder.Finder;
+using Ocelot.Responses;
+
+namespace NihFix.Ocelot.MixedRouting
+{
+    public class MixedDownstreamRouteProvider : IDownstreamRouteProvider
+    {
+        private readonly Lazy<Dictionary<string, IDownstreamRouteProvider>> _providers;
+
+        public MixedDownstreamRouteProvider(IServiceProvider provider)
+        {
+            _providers = new Lazy<Dictionary<string, IDownstreamRouteProvider>>(() => provider
+                .GetServices<IDownstreamRouteProvider>()
+                .ToDictionary(
+                    x => x.GetType().Name));
+        }
+
+        ///<inheritdoc/>
+        public Response<DownstreamRoute> Get(string upstreamUrlPath, string upstreamQueryString,
+            string upstreamHttpMethod,
+            IInternalConfiguration configuration, string upstreamHost)
+        {
+            var finder = _providers.Value[nameof(DownstreamRouteFinder)];
+            var creator = _providers.Value[nameof(DownstreamRouteCreator)];
+            var finderResult = finder.Get(upstreamUrlPath, upstreamQueryString, upstreamHttpMethod, configuration,
+                upstreamHost);
+            return finderResult.IsError
+                ? creator.Get(upstreamUrlPath, upstreamQueryString, upstreamHttpMethod, configuration, upstreamHost)
+                : finderResult;
+        }
+    }
+}
---
+++ NihFix.Ocelot.MixedRouting/MixedDownstreamRouteProviderFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Configuration;
+using Ocelot.DownstreamRouteFinder.Finder;
+using Ocelot.Logging;
+
+namespace NihFix.Ocelot.MixedRouting
+{
+    public class MixedDownstreamRouteProviderFactory : IDownstreamRouteProviderFactory
+    {
+        private readonly Dictionary<string, IDownstreamRouteProvider> _providers;
+        private readonly IOcelotLogger _logger;
+
+        public MixedDownstreamRouteProviderFactory(IServiceProvider provider, IOcelotLoggerFactory factory)
+        {
+            _logger = factory.CreateLogger<DownstreamRouteProviderFactory>();
+            _providers = provider.GetServices<IDownstreamRouteProvider>()
+                .ToDictionary(
+                    x => x.GetType().Name);
+        }
+
+        ///<inheritdoc/>
+        public IDownstreamRouteProvider Get(IInternalConfiguration config)
+        {
+            var hasRoutesInConfig = config.ReRoutes.Any() && !config.ReRoutes.All(
+                x => string.IsNullOrEmpty(x.UpstreamTemplatePattern?.OriginalValue));
+            var isServiceDiscovery = IsServiceDiscovery(config.ServiceProviderConfiguration);
+            if (hasRoutesInConfig && isServiceDiscovery)
+            {
+                _logger.LogInformation(
+                    "Selected MixedDownstreamRouteProvider as DownstreamRouteProvider for this request");
+                return _providers[nameof(MixedDownstreamRouteProvider)];
+            }
+            else if (isServiceDiscovery && !hasRoutesInConfig)
+            {
+                _logger.LogInformation("Selected DownstreamRouteCreator as DownstreamRouteProvider for this request");
+                return _providers[nameof(DownstreamRouteCreator)];
+            }
+
+            _logger.LogInformation("Selected DownstreamRouteFinder as DownstreamRouteProvider for this request");
+
+            return _providers[nameof(DownstreamRouteFinder)];
+        }
+
+        private bool IsServiceDiscovery(ServiceProviderConfiguration config) => !string.IsNullOrEmpty(config?.Host) &&
+            config != null && (config.Port > 0 && !string.IsNullOrEmpty(config.Type));
+    }
+}
