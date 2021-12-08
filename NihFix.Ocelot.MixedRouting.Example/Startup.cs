d03bd32e5e04e9ab2d99d02539a3f8c3404d356d
ALEKSEY KAPUSTYANENKO <lehkap@hotmail.com>
2021-12-08 22:16:29 +0400

    Add di registration

--- NihFix.Ocelot.MixedRouting.Example/Startup.cs
+++ NihFix.Ocelot.MixedRouting.Example/Startup.cs
@@ -18,9 +18,10 @@ namespace NihFix.Ocelot.MixedRouting.Example
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot().AddConsul();
-            
-
+            services
+                .AddOcelot()
+                .AddConsul()
+                .AddMixedRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ NihFix.Ocelot.MixedRouting/OcelotBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.DownstreamRouteFinder.Finder;
+
+namespace NihFix.Ocelot.MixedRouting
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add ability to combine dynamic routing(with service discovery)
+        /// and routing from configuration file.
+        /// </summary>
+        /// <param name="ocelotBuilder">Ocelot builder.</param>
+        /// <returns>Ocelot builder.</returns>
+        public static IOcelotBuilder AddMixedRouting(this IOcelotBuilder ocelotBuilder)
+        {
+            ocelotBuilder.Services
+                .RemoveAll(typeof(IDownstreamRouteProviderFactory))
+                .AddSingleton<IDownstreamRouteProviderFactory, MixedDownstreamRouteProviderFactory>()
+                .AddSingleton<IDownstreamRouteProvider, MixedDownstreamRouteProvider>();
+            return ocelotBuilder;
+        }
+    }
+}
