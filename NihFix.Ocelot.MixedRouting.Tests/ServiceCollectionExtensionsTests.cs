47c94a0862d64a4a67677faf27184e394a7bdcae
ALEKSEY KAPUSTYANENKO <lehkap@hotmail.com>
2021-12-08 22:47:32 +0400

    Add unit test

---
+++ NihFix.Ocelot.MixedRouting.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Ocelot.DependencyInjection;
+using Ocelot.DownstreamRouteFinder.Finder;
+using Xunit;
+
+namespace NihFix.Ocelot.MixedRouting.Tests
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        private ServiceProvider _serviceProvider;
+
+        public ServiceCollectionExtensionsTests()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfiguration>(Substitute.For<IConfiguration>());
+            serviceCollection.AddOcelot().AddMixedRouting();
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        [Fact]
+        public async Task
+            AddMixedRouting_ResolveIDownstreamRouteProviderFactory_ShouldBeMixedDownstreamRouteProviderFactory()
+        {
+            var service = _serviceProvider.GetRequiredService<IDownstreamRouteProviderFactory>();
+
+            Assert.IsType<MixedDownstreamRouteProviderFactory>(service);
+        }
+
+        [Fact]
+        public async Task
+            AddMixedRouting_ResolveIDownstreamRouteProviderList_ShouldContainsMixedDownstreamRouteProvider()
+        {
+            var providers = _serviceProvider.GetServices<IDownstreamRouteProvider>();
+
+            var mixedProvider = providers.FirstOrDefault(p => p.GetType() == typeof(MixedDownstreamRouteProvider));
+            Assert.NotNull(mixedProvider);
+        }
+    }
+}
